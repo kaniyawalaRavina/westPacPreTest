c7e01e5d258f4fdd38f2027c049926e0aee0c7a7
ravina <ravina.kaniyawala@gmail.com>
2021-05-30 11:13:59 +1200

    added project

---
+++ PreTestTask/PreTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreTestTask
+//
+//  Created by Riya Kaniyawala on 30/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
